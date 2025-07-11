bb584ed32c72850900cbf0660fa951ec2ad46393
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-07-11 15:49:02 +0100

    Update bin

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,8 +1,5 @@
 import { runSelectedScript } from "../src/util/parsePackageScripts.mjs";
 import { logTimestampArrow } from "@gld5000k/timestamp";
 
-export async function runSelector() {
   logTimestampArrow();
   await runSelectedScript();
-}
-runSelector();
